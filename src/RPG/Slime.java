763c3631f8e341073cc4f51396f7b6fa8a439159
UwUBots <staffenjosh@gmail.com>
2021-04-16 17:09:28 -0400

    User input Wroking

---
+++ src/RPG/Slime.java
@@ -0,0 +1,18 @@
+package RPG;
+
+import java.awt.*;
+
+public class Slime extends GameObject {
+
+    public Slime(int x, int y, ID id) {
+        super(x, y, id);
+    }
+
+    public void tick() {
+
+    }
+
+    public void render(Graphics g) {
+
+    }
+}
